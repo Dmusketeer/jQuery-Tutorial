fed0fad358b0db439289f110af2db247d4e40f56
Dmusketeer <ardmountronics@gmail.com>
2021-08-07 09:45:53 +0530

    jquery logic

---
+++ Day3/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#circle').click(function(){
+        alert('you clicked on circle')
+    });
+    $('#square').click(function () {
+        alert('you clicked on Square')
+    });
+})
+
